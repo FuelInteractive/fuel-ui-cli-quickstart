5816ed77a005307c105427895efc40e755034d8b
coryshaw1 <cory.shaw.dev@gmail.com>
2016-06-22 14:44:20 -0400

    update: Angular 2 RC3 and fuel-ui 0.3.6

--- src/app/fuel-ui-cli-quickstart.component.ts
+++ src/app/fuel-ui-cli-quickstart.component.ts
@@ -6,13 +6,13 @@ import {DATE_PICKER_PROVIDERS, DateRange, Collapse} from 'fuel-ui/fuel-ui';
   selector: 'fuel-ui-cli-quickstart-app',
   templateUrl: 'fuel-ui-cli-quickstart.component.html',
   styleUrls: ['fuel-ui-cli-quickstart.component.css'],
-  directives: [DATE_PICKER_PROVIDERS, Collapse]
+  directives: [DATE_PICKER_PROVIDERS],
+  animations: [Collapse(300)]
 })
 export class FuelUiCliQuickstartAppComponent {
   title = 'fuel-ui-cli-quickstart works!';
   name: string = 'World';
   collapsed: boolean = false;
-  duration: number = 500;
   
   dateRangePickerValue: DateRange;
   
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { provideForms, disableDeprecatedForms } from '@angular/forms';
 import { FuelUiCliQuickstartAppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(FuelUiCliQuickstartAppComponent);
+bootstrap(FuelUiCliQuickstartAppComponent, [
+  provideForms(),
+  disableDeprecatedForms()
+]);
 
